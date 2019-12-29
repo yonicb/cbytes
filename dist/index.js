d55f725de5e040d6f61f019c1a0cc28a8e6db038
yonicb <yonicb20@gmail.com>
2019-12-29 16:19:41 -0500

    feat: 🎸 Created build main files

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+/*!
+ * Cbytes v1.0.0 (https://github.com/yonicb/cbytes)
+ * Copyright 2019 The Cbytes Authors
+ * Copyright 2019 Yoni Calsin <@yonicb>.
+ * Licensed under MIT (https://github.com/yonicb/cbytes/blob/master/LICENSE)
+ */
