989ec81c0acd83cdbb292067a5abfeae3e692c0b
yonicb <yonicb20@gmail.com>
2019-12-30 13:54:26 -0500

    test: 💍 Update to number test file

--- test/to-number.spec.ts
+++ test/to-number.spec.ts
@@ -9,9 +9,9 @@ test('format 1KB to 1024 ', () => {
 });
 
 /**
- * FROM: 1KB
- * TO: 1024
+ * FROM: 4.5GB
+ * TO: 4831838208
  */
-test('format 1KB to 1024 ', () => {
-    expect(toNumber('1KB')).toBe(1024);
+test('format 4.5GB to 4831838208 ', () => {
+    expect(toNumber('4.5GB')).toBe(4831838208);
 });
